054f324e9ba7281b8cf884112a6966cc82ba72ac
Rafail Medzhidov <rafayt323@gmail.com>
2023-11-03 19:19:25 +0300

    feat(day): add "word" (#13)
    * feat(word): add word model
    
    * chore: update ignores in django migration linter

--- src/server/apps/days/admin.py
+++ src/server/apps/days/admin.py
@@ -1,9 +1,22 @@
 """Admin settings for `User` model."""
 from django.contrib import admin
 
-from server.apps.days.models import StudyDay
+from server.apps.days import models
 
 
-@admin.register(StudyDay)
+class TabularWordAdmin(admin.TabularInline):
+    """`Word` tabular representation in admin-panel."""
+
+    model = models.Word
+
+
+@admin.register(models.Word)
+class WordAdmin(admin.ModelAdmin):
+    """`Word` representation in admin-panel."""
+
+
+@admin.register(models.StudyDay)
 class StudyDayAdmin(admin.ModelAdmin):
     """`StudyDay` representation in admin-panel."""
+
+    inlines = (TabularWordAdmin,)
--- src/server/apps/days/models.py
+++ src/server/apps/days/models.py
@@ -1,8 +1,12 @@
 """Module with models to representate days."""
 import uuid
+from typing import Final
 
 from django.db import models
 from django.utils.translation import gettext_lazy
+
+_MEANING_LENGTH: Final = 40
+_TRANSLATION_LENGTH: Final = 70
 
 
 class StudyDay(models.Model):
@@ -27,3 +31,38 @@ class StudyDay(models.Model):
     def __str__(self) -> str:
         """String representation of study day with day's number."""
         return 'Day №{0}'.format(self.title)
+
+
+class Word(models.Model):
+    """Word of study day with required fields."""
+
+    id = models.UUIDField(
+        verbose_name=gettext_lazy('Id'),
+        primary_key=True,
+        default=uuid.uuid4,
+    )
+    meaning = models.CharField(
+        max_length=_MEANING_LENGTH,
+        verbose_name=gettext_lazy('Meaning'),
+        blank=False,
+    )
+    translation = models.CharField(
+        max_length=_TRANSLATION_LENGTH,
+        verbose_name=gettext_lazy('Translation'),
+        blank=False,
+    )
+    day = models.ForeignKey(
+        StudyDay,
+        verbose_name=gettext_lazy('Day'),
+        related_name='study_days',
+        on_delete=models.CASCADE,
+    )
+
+    class Meta:
+        db_table = 'words'
+        verbose_name = gettext_lazy('Word')
+        verbose_name_plural = gettext_lazy('Words')
+
+    def __str__(self) -> str:
+        """String representation of word with it's korean meaning."""
+        return self.meaning
